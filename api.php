e1dd5fd8b78f3ed372df6ab54c74b3f9d94a5672
Joaquin <joaquinvergara070@gmail.com>
2022-06-23 09:55:28 -0300

    changed api to work with spreadsheets

--- api.php
+++ api.php
@@ -5,5 +5,5 @@ include_once "connect.php";
 $cant_query = $conn->query("SELECT * FROM fdp");
 $total_count = $cant_query->num_rows;
 
-echo json_encode(array('cantidad' => $total_count));
+echo $total_count;
 ?>
