332ce6b4c41e3eb2d3b47ef56d9ee803d9394adf
Joaquin <joaquinvergara070@gmail.com>
2022-07-06 21:18:39 -0300

    platita

--- api.php
+++ api.php
@@ -3,7 +3,7 @@ include_once "connect.php";
 
 
 $cant_query = $conn->query("SELECT * FROM fdp");
-$total_count = $cant_query->num_rows;
+$total_count = $cant_query->num_rows - 4;
 
 echo $total_count;
 ?>
