bb9a6ece0a0ae1c04534e2bfc4aa6a419f26952b
Joaquin Vergara Araya <joaquinvergara070@gmail.com>
2022-08-12 10:46:56 -0300

    Update api.php

--- api.php
+++ api.php
@@ -3,7 +3,7 @@ include_once "connect.php";
 
 
 $cant_query = $conn->query("SELECT * FROM fdp");
-$total_count = $cant_query->num_rows - 27;
+$total_count = $cant_query->num_rows;
 
 echo $total_count;
 ?>
