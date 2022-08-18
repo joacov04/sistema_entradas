c89981363678ffdfce24f58663f396e056783691
Joaquin <joaquinvergara070@gmail.com>
2022-08-18 14:32:22 -0300

    a instacan le cuesta

--- camera.php
+++ camera.php
@@ -6,8 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style/main.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script type="text/javascript" src="https://rawgit.com/schmich/instascan-builds/master/instascan.min.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <script type="text/javascript" src="src/instascan.min.js"></script>
     <title>Generar entradas</title>
 </head>
 <body>
