4605b90f32c2d08637c872c30176b533ff36c9b7
Joaquin <joaquinvergara070@gmail.com>
2022-08-31 22:10:22 -0300

    ajax scanning is working

---
+++ app/getTokenInfo.php
@@ -0,0 +1,18 @@
+<?php
+
+include_once "connect.php";
+if(isset($_GET['token'])) {
+    $token = mysqli_real_escape_string($conn, $_GET['token']);
+    $sql = $conn->query("SELECT * FROM fdp WHERE token='".$token."' ");
+
+    $row_cnt = $sql->num_rows;
+    if ($row_cnt > 0) {
+        $row = $sql->fetch_array(MYSQLI_ASSOC);
+        echo json_encode($row);
+    } else {
+        echo json_encode(2);
+    }
+}
+
+
+?>
