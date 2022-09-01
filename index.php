b5fb41900d45fb8ef665e5fe282a8925e9bc5000
Joaquin <joaquinvergara070@gmail.com>
2022-09-01 17:34:46 -0300

    moved script from html to file

--- index.php
+++ index.php
@@ -11,35 +11,11 @@
 </head>
 <body>
     
-    <script type="text/javascript">
-        $(document).ready(function(){
-            var maxField = 20; //Input fields increment limitation
-            var addButton = $('.add_button'); //Add button selector
-            var wrapper = $('.field_wrapper'); //Input field wrapper
-            var fieldHTML = '<div><input type="text" placeholder="Nombre y Apellido" name="field_name[]" value=""/><a href="javascript:void(0);" class="remove_button">-</a></div>'; //New input field html 
-            var x = 1; //Initial field counter is 1
-            
-            //Once add button is clicked
-            $(addButton).click(function(){
-                //Check maximum number of input fields
-                if(x < maxField){ 
-                    x++; //Increment field counter
-                    $(wrapper).append(fieldHTML); //Add field html
-                }
-            });
-            
-            //Once remove button is clicked
-            $(wrapper).on('click', '.remove_button', function(e){
-                e.preventDefault();
-                $(this).parent('div').remove(); //Remove field html
-                x--; //Decrement field counter
-            });
-        });
-        </script>
+    <script type="text/javascript" src="src/multipleNames.js"></script>
 
     <div>
         <h1>ENTRADAS</h1>
-        <a href="buscar.php"><img src="img/lupa.png" id="lupa"></a>
+        <img src="img/lupa.png" id="lupa">
 
         <form  method="post">                
             <div class="field_wrapper flex">
---
+++ src/multipleNames.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+    var maxField = 20; //Input fields increment limitation
+    var addButton = $('.add_button'); //Add button selector
+    var wrapper = $('.field_wrapper'); //Input field wrapper
+    var fieldHTML = '<div><input type="text" placeholder="Nombre y Apellido" name="field_name[]" value=""/><a href="javascript:void(0);" class="remove_button">-</a></div>'; //New input field html 
+    var x = 1; //Initial field counter is 1
+    
+    //Once add button is clicked
+    $(addButton).click(function(){
+        //Check maximum number of input fields
+        if(x < maxField){ 
+            x++; //Increment field counter
+            $(wrapper).append(fieldHTML); //Add field html
+        }
+    });
+    
+    //Once remove button is clicked
+    $(wrapper).on('click', '.remove_button', function(e){
+        e.preventDefault();
+        $(this).parent('div').remove(); //Remove field html
+        x--; //Decrement field counter
+    });
+});
