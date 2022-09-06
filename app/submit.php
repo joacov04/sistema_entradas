382b411668658b55eb0a4a1f026df45afc8d9f1e
Joaquin <joaquinvergara070@gmail.com>
2022-09-06 20:06:43 -0300

    wip: changin hrefs to share button

--- app/submit.php
+++ app/submit.php
@@ -23,7 +23,7 @@ if(isset($_POST['submit'])){
         $count = 0;
         while (false !== ($entry = readdir($handle))){
              if($entry != '.' && $entry != '..') {
-                $script = "{const blob=fetch('qr/$entry');navigator.share({files:[blob]})}";
+                $script = "if(navigator.share){const blob=fetch('qr/$entry');navigator.share({files:[blob]})}else {console.log(2)}";
                 echo $script;
                 echo "<a class='qrs' onclick=\"$script\">$entry</a> \n";
                 $count++;
