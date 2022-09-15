9b5e03060b9f779c82052c065e6eb5545d3ae7f7
Joaquin <joaquinvergara070@gmail.com>
2022-09-15 13:05:46 -0300

    moved files

--- app/buscar.php
+++ app/buscar.php
@@ -4,18 +4,18 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style/main.css">
+    <link rel="stylesheet" href="../style/main.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
-    <script type="text/javascript" src="src/main.js"></script>
+    <script type="text/javascript" src="../src/main.js"></script>
     <title>Buscar entrada</title>
 
 </head>
 <body>
 
     <div class='flex'>
-        <span class="material-symbols-outlined icons" id="arrow_back" onclick="window.location.href='index.php'">arrow_back</span>
+        <span class="material-symbols-outlined icons" id="arrow_back" onclick="window.location.href='app/index.php'">arrow_back</span>
         
         <div class="box">
             <table id="names">
@@ -38,7 +38,7 @@
                 </thead>
                 <tbody>
                 <?php
-                    include("app/backend_buscar.php");
+                    include("backend_buscar.php");
                 ?>
                 </tbody>
             </table>
--- app/camera.php
+++ app/camera.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style/main.css">
+    <link rel="stylesheet" href="../style/main.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <script type="text/javascript" src="https://rawgit.com/schmich/instascan-builds/master/instascan.min.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
@@ -12,7 +12,7 @@
     <title>Escanear QR</title>
 </head>
 <body class="flex">
-    <span class="material-symbols-outlined icons" id="arrow_back" onclick="window.location.href='index.php'">arrow_back</span>
+    <span class="material-symbols-outlined icons" id="arrow_back" onclick="window.location.href='../index.php'">arrow_back</span>
     <h2 id="text">Scan QR</h2>
     <div id="validation" class="modal">
         <span class="material-symbols-outlined">
@@ -30,6 +30,6 @@
     <br><br><br><br><br><br><br><br><br><br><br><br><br><br>
     <a id="bottom-link" target="_blank" href="https://github.com/joacov04/sistema_entradas"><i class="fa fa-github" style="font-size:24px"></i> Página oficial</a>
     
-    <script type="text/javascript" src="src/camera.js"></script>
+    <script type="text/javascript" src="../src/camera.js"></script>
   </body>
 </html>
