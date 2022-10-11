1dc9061ad7404b77ecfc813398f476b9a6feae32
Joaquin <joaquinvergara070@gmail.com>
2022-10-11 11:42:30 -0300

    less cpu usage and scan codes faster

--- src/camera.js
+++ src/camera.js
@@ -14,7 +14,7 @@ $(document).ready(function() {
 
 function ready(being_called=0) {
     $('.modal').css("display", "none");
-    let scanner = new Instascan.Scanner({ video: document.getElementById('preview'), mirror: false });
+    let scanner = new Instascan.Scanner({ video: document.getElementById('preview'), mirror: false, backgroundScan: false, scanPeriod: 0.1 });
         Instascan.Camera.getCameras().then(function (cameras) {
             if(being_called == 0) {
                 if (cameras.length > 0) {
