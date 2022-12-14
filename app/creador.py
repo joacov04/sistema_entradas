d5607f0033881d06d6fb25c4deb01c48d84a190f
Joaquin <joaquinvergara070@gmail.com>
2022-12-14 14:11:10 -0300

    fdp vol3

--- app/creador.py
+++ app/creador.py
@@ -13,6 +13,7 @@ from PIL import ImageDraw
 from PIL import ImageFont
 # to do de aca
 # que se pueda usar cualquier foto en vez de solo la que se llama back.png
+IMG_PATH = 'app/vol3.png'
 
 with open("app/credentials.inc", "r", encoding='ascii') as connection_file:
     credentials = {"user": "", "pass": "", "base": "", "host": "", "table": ""}
@@ -44,12 +45,12 @@ def overlay(qr_path, token):
     Pastes the QR specified into the background image, with the token below.
     """
     font = ImageFont.truetype(font='app/Calibri.ttf', size=45)
-    background = Image.open('app/lucky.jpeg')
+    background = Image.open(IMG_PATH)
     (background_width, background_heigth) = background.size
 
     qr_img = Image.open(qr_path)
     (qr_width, qr_heigth) = qr_img.size
-    heigth_to_paste = int(background_heigth/2.3)
+    heigth_to_paste = int(background_heigth/2.6)
     width_to_paste = int((background_width-qr_width)/2)
 
     background.paste(qr_img, (width_to_paste, heigth_to_paste))
@@ -66,7 +67,7 @@ def make_qr(token, nombre):
     """
     Makes the QR code with the specified token and saves it as the person name.
     """
-    qr = qrcode.QRCode(version=1, box_size=17, border=1)
+    qr = qrcode.QRCode(version=1, box_size=25, border=1)
     qr.add_data(token)
     qr.make(fit=True)
     img = qr.make_image(fill='black', back_color='white')
