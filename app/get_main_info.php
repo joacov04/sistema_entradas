a54a57f79577fb77bd1d1efaa754cbd2dccfdf13
Joaquin <joaquinvergara070@gmail.com>
2022-12-16 10:20:18 -0300

    get main accounting info

--- app/get_main_info.php
+++ app/get_main_info.php
@@ -7,14 +7,22 @@ if ($user != 'joaquin' && $user != 'payo') {
 }
 
 $MARGIN_PERCENT = 0.1;
+$PRICE = 1000;
 
 $sql = $conn->query("SELECT COUNT(*) AS Total FROM ".$table);
 $row_cnt = $sql->num_rows;
 while($row = $sql->fetch_array(MYSQLI_ASSOC)) {
     $total_entradas = $row['Total'];
-    $total_plata = number_format(((int)$total)*1000);
-    array();
+    $total_plata = number_format(((int)$total)*$PRICE);
+    $margen = (int)$total*$PRICE*$MARGIN_PERCENT;
 
+    $info = [
+        "entradas"  => $total_entradas,
+        "plata"     => $total_plata,
+        "margen"    => $margen,
+    ];
+
+    echo json_encode($info);
 
 }
 
