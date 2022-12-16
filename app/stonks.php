b91c7f11f75d86071a626bce749c5387749ebba2
Joaquin <joaquinvergara070@gmail.com>
2022-12-16 11:00:00 -0300

    order seller table

--- app/stonks.php
+++ app/stonks.php
@@ -2,7 +2,7 @@
 include_once "connect.php";
 
 
-$sql = $conn->query("SELECT vendedor, COUNT(*) AS Total FROM ".$table." GROUP BY vendedor ORDER BY Total");
+$sql = $conn->query("SELECT vendedor, COUNT(*) AS Total FROM ".$table." GROUP BY vendedor ORDER BY Total DESC");
 $row_cnt = $sql->num_rows;
 while($row = $sql->fetch_array(MYSQLI_ASSOC)) {
     $total = $row['Total'];
