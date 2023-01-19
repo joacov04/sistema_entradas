4ab383d79688b5a3718e419dff89e30dcf409e8b
Joaquin <joaquinvergara070@gmail.com>
2023-01-19 15:17:22 -0300

    sellers can see only their sells

--- app/backend_buscar.php
+++ app/backend_buscar.php
@@ -5,7 +5,6 @@ include_once "connect.php";
 $sql = $conn->query("select * from bios_persons join bios_tokens on bios_persons.token=bios_tokens.token;");
 $row_cnt = $sql->num_rows;
 while($row = $sql->fetch_array(MYSQLI_ASSOC)) {
-    if($_SERVER['PHP_AUTH_USER'] == 'joaquin' || $_SERVER['PHP_AUTH_USER'] == 'feli') {
     $nombre = $row['nombre'];
     $token = $row['qr_token'];
     $vendedor = $row['vendedor'];
@@ -17,9 +16,6 @@ while($row = $sql->fetch_array(MYSQLI_ASSOC)) {
     } else {
         $usada = 'NO';
         $accion = 'Usada';
-    }
-    } else {
-        if($row['vendedor'] == $_SERVER['PHP_AUTH_USER']) {
             $nombre = $row['nombre'];
             $token = $row['qr_token'];
             $vendedor = $row['vendedor'];
@@ -33,9 +29,7 @@ while($row = $sql->fetch_array(MYSQLI_ASSOC)) {
                 $accion = 'Usada';
             }
 
-        }
-    }
-
+    if($row['vendedor'] == $_SERVER['PHP_AUTH_USER'] || $_SERVER['PHP_AUTH_USER'] == 'joaquin' || $_SERVER['PHP_AUTH_USER'] == 'feli' ) {
     echo "<tr>";
     echo "<td>".$nombre."</td>";
     echo "<td>".$token."</td>";
