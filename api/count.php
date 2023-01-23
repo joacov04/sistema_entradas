dbb669791860b03f32277483888219002fc5c764
Joaquin <joaquinvergara070@gmail.com>
2023-01-23 19:32:53 -0300

    updated api

--- api/count.php
+++ api/count.php
@@ -3,7 +3,7 @@ include_once "../app/connect.php";
 
 
 $cant_query = $conn->query("SELECT sum(cantidad) AS Total FROM bios_tokens");
-$total_count = $cant_query->fetch_assoc();
+$total_count = $cant_query->fetch_array(MYSQLI_ASSOC);
 
 echo $total_count['Total'];
 ?>
