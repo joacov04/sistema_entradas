278454486e19b40f1647283cd857552cfcec1f94
Joaquin <joaquinvergara070@gmail.com>
2023-01-30 21:33:24 -0300

    moved login location

--- login.php
+++ login.php
@@ -3,7 +3,7 @@
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,700,0,0" />
-    <link rel="stylesheet" href="ticket.css">
+    <link rel="stylesheet" href="verification/ticket.css">
     <title>Document</title>
 </head>
 <body>
